53ba7ebb8e33d57e9fb9833a2287efd50b219649
Mami K <49592804+carveler@users.noreply.github.com>
2024-06-08 23:09:15 +0200

    feat: add argparse for main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from movie_app import MovieApp
 from storage import StorageCsv
-# from storage import StorageJson
-
+from storage import StorageJson
+import argparse
 
 def main():
-    # storage = storage_json.StorageJson("./movies.json")
-    storage = StorageCsv("./data/movies.csv")
+    parser = argparse.ArgumentParser(description="movie file name")
+    parser.add_argument("file_name", help="movie file name csv or json")
+    args = parser.parse_args() 
+
+    storage = None
+    
+    if "csv" in args.file_name:
+        storage = StorageCsv(f"./data/{args.file_name}")
+    elif "json" in args.file_name:
+        storage = StorageJson(f"./data/{args.file_name}")
+
     movie_app = MovieApp(storage)
     movie_app.run()
 
